2ba9053e613d0518b4aa68eb71f16bb23e78b11e
Elton Zheng <elton.zheng.sh@gmail.com>
2022-04-12 23:31:50 +0800

    completed lesson 7

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,31 @@ const count = ref(0);
 const increment = () => count.value++;
 
 const text = ref('');
+
+const awesome = ref(true);
+const toggle = () => {
+  awesome.value = !awesome.value;
+};
+
+let id = 0;
+const newTodo = ref('');
+const todos = ref([
+  { id: id++, text: 'Learn Vue', done: false },
+  { id: id++, text: 'Learn React', done: false },
+  { id: id++, text: 'Learn Nuxt', done: false },
+]);
+
+const addTodo = () => {
+  todos.value.push({
+    id: id++,
+    text: newTodo.value,
+    done: false,
+  });
+  newTodo.value = '';
+};
+
+const removeTodo = (todo) =>
+  (todos.value = todos.value.filter((t) => t.id !== todo.id));
 </script>
 
 <template>
@@ -49,6 +74,45 @@ const text = ref('');
         class="rounded border p-1 text-sm"
       />
       <p>{{ text.toUpperCase() }}</p>
+    </div>
+
+    <div class="w-1/4 text-center">
+      <p v-if="awesome">Vue is awesome!</p>
+      <p v-else>oh, no. 🤣</p>
+      <button @click="toggle" class="w-24 rounded bg-slate-600 px-1 text-white">
+        toggle
+      </button>
+    </div>
+
+    <div>
+      <input
+        type="text"
+        class="rounded border border-neutral-400 p-1 text-sm"
+        v-model="newTodo"
+        @keyup.enter="addTodo"
+        placeholder="Type new todo"
+      />
+      <button
+        @click="addTodo"
+        class="ml-2 rounded bg-neutral-600 px-2 text-white"
+      >
+        Add todo
+      </button>
+      <ul>
+        <li
+          v-for="todo in todos"
+          :key="todo.id"
+          class="mt-2 flex items-center text-sm text-neutral-600"
+        >
+          <p>{{ todo.text }}</p>
+          <button
+            @click="removeTodo(todo)"
+            class="ml-2 rounded border bg-amber-800 px-2 text-sm text-white"
+          >
+            x
+          </button>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
